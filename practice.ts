e082fe75d14d056256c2681971283eb6fc88144a
anishsharma21 <anisharma2103@gmail.com>
2024-07-17 19:14:21 +1000

    Initial commit - exploring typescript, javascript with the DOM, etc

---
+++ practice.ts
@@ -0,0 +1,44 @@
+type Book = {
+  title: string;
+  author: string;
+  isbn: string;
+  stockQuantity: number;
+};
+
+type Inventory = {
+  books: Array<Book>;
+};
+
+class BookStore {
+  inventory: Inventory;
+
+  constructor() {
+    this.inventory = { books: [] };
+  }
+
+  addBook(book: Book) {
+    const existingBookIndex = this.inventory.books.findIndex(
+      (b) => b.isbn === book.isbn
+    );
+    if (existingBookIndex > -1) {
+      this.inventory.books[existingBookIndex].stockQuantity +=
+        book.stockQuantity;
+    } else {
+      this.inventory.books.push(book);
+    }
+  }
+}
+
+type User = {
+  name: string;
+  age: number;
+  sayHello: () => void;
+};
+
+let user: User = {
+  name: "Anish",
+  age: 20,
+  sayHello() {
+    console.log("Hello");
+  },
+};
